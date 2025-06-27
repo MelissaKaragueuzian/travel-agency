dd2d1e931dbfffd5719b70ac50ec267941986545
jess <154530260+jesss77@users.noreply.github.com>
2025-06-27 12:48:08 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace TravelAgency_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label labelAppName;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelSubtitle;
+        private Guna.UI2.WinForms.Guna2Panel mainPanel;
+        private Guna.UI2.WinForms.Guna2TextBox txtSearch;
+        private Guna.UI2.WinForms.Guna2Button btnCategory;
+        private Guna.UI2.WinForms.Guna2Button btnStatus;
+        private Guna.UI2.WinForms.Guna2Button btnDate;
+        private Guna.UI2.WinForms.Guna2DataGridView dataGridViewEntries;
+        private Guna.UI2.WinForms.Guna2Button btnAdd;
+        private Guna.UI2.WinForms.Guna2Button btnUpdate;
+        private Guna.UI2.WinForms.Guna2Button btnDelete;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.labelAppName = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelSubtitle = new System.Windows.Forms.Label();
+            this.mainPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.txtSearch = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnCategory = new Guna.UI2.WinForms.Guna2Button();
+            this.btnStatus = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDate = new Guna.UI2.WinForms.Guna2Button();
+            this.dataGridViewEntries = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.btnAdd = new Guna.UI2.WinForms.Guna2Button();
+            this.btnUpdate = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDelete = new Guna.UI2.WinForms.Guna2Button();
+            this.panelTop.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEntries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.White;
+            this.panelTop.Controls.Add(this.labelAppName);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1371, 64);
+            this.panelTop.TabIndex = 1;
+            // 
+            // labelAppName
+            // 
+            this.labelAppName.AutoSize = true;
+            this.labelAppName.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.labelAppName.Location = new System.Drawing.Point(23, 16);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(200, 37);
+            this.labelAppName.TabIndex = 0;
+            this.labelAppName.Text = "Data Manager";
+            this.labelAppName.Click += new System.EventHandler(this.labelAppName_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.Location = new System.Drawing.Point(46, 85);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(212, 46);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "Data Entries";
+            // 
+            // labelSubtitle
+            // 
+            this.labelSubtitle.AutoSize = true;
+            this.labelSubtitle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelSubtitle.ForeColor = System.Drawing.Color.Gray;
+            this.labelSubtitle.Location = new System.Drawing.Point(46, 128);
+            this.labelSubtitle.Name = "labelSubtitle";
+            this.labelSubtitle.Size = new System.Drawing.Size(265, 23);
+            this.labelSubtitle.TabIndex = 3;
+            this.labelSubtitle.Text = "Manage and view all data entries.";
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BorderRadius = 30;
+            this.mainPanel.Controls.Add(this.txtSearch);
+            this.mainPanel.Controls.Add(this.btnCategory);
+            this.mainPanel.Controls.Add(this.btnStatus);
+            this.mainPanel.Controls.Add(this.btnDate);
+            this.mainPanel.Controls.Add(this.btnAdd);
+            this.mainPanel.Controls.Add(this.btnUpdate);
+            this.mainPanel.Controls.Add(this.btnDelete);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.mainPanel.Location = new System.Drawing.Point(0, 64);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1371, 736);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderRadius = 12;
+            this.txtSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearch.DefaultText = "";
+            this.txtSearch.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtSearch.Location = new System.Drawing.Point(446, 142);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PlaceholderText = "Search entries...";
+            this.txtSearch.SelectedText = "";
+            this.txtSearch.Size = new System.Drawing.Size(457, 43);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnCategory
+            // 
+            this.btnCategory.BorderRadius = 12;
+            this.btnCategory.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.btnCategory.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnCategory.ForeColor = System.Drawing.Color.Black;
+            this.btnCategory.Location = new System.Drawing.Point(446, 206);
+            this.btnCategory.Name = "btnCategory";
+            this.btnCategory.Size = new System.Drawing.Size(137, 38);
+            this.btnCategory.TabIndex = 1;
+            this.btnCategory.Text = "Category";
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.BorderRadius = 12;
+            this.btnStatus.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.btnStatus.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnStatus.ForeColor = System.Drawing.Color.Black;
+            this.btnStatus.Location = new System.Drawing.Point(594, 206);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(137, 38);
+            this.btnStatus.TabIndex = 2;
+            this.btnStatus.Text = "Status";
+            // 
+            // btnDate
+            // 
+            this.btnDate.BorderRadius = 12;
+            this.btnDate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.btnDate.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDate.ForeColor = System.Drawing.Color.Black;
+            this.btnDate.Location = new System.Drawing.Point(743, 206);
+            this.btnDate.Name = "btnDate";
+            this.btnDate.Size = new System.Drawing.Size(137, 38);
+            this.btnDate.TabIndex = 3;
+            this.btnDate.Text = "Date";
+            // 
+            // dataGridViewEntries
+            // 
+            this.dataGridViewEntries.AllowUserToAddRows = false;
+            this.dataGridViewEntries.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.dataGridViewEntries.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewEntries.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridViewEntries.ColumnHeadersHeight = 29;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(226)))), ((int)(((byte)(243)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewEntries.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridViewEntries.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridViewEntries.Location = new System.Drawing.Point(69, 256);
+            this.dataGridViewEntries.Name = "dataGridViewEntries";
+            this.dataGridViewEntries.ReadOnly = true;
+            this.dataGridViewEntries.RowHeadersVisible = false;
+            this.dataGridViewEntries.RowHeadersWidth = 51;
+            this.dataGridViewEntries.Size = new System.Drawing.Size(1029, 373);
+            this.dataGridViewEntries.TabIndex = 4;
+            this.dataGridViewEntries.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridViewEntries.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dataGridViewEntries.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dataGridViewEntries.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dataGridViewEntries.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dataGridViewEntries.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridViewEntries.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.Black;
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridViewEntries.ThemeStyle.HeaderStyle.Height = 29;
+            this.dataGridViewEntries.ThemeStyle.ReadOnly = true;
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.Height = 22;
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(226)))), ((int)(((byte)(243)))));
+            this.dataGridViewEntries.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BorderRadius = 12;
+            this.btnAdd.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(133)))), ((int)(((byte)(244)))));
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(474, 64);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(114, 43);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BorderRadius = 12;
+            this.btnUpdate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnUpdate.ForeColor = System.Drawing.Color.Black;
+            this.btnUpdate.Location = new System.Drawing.Point(599, 64);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(114, 43);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BorderRadius = 12;
+            this.btnDelete.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(241)))));
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(725, 64);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(114, 43);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 800);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelSubtitle);
+            this.Controls.Add(this.dataGridViewEntries);
+            this.Name = "Form1";
+            this.Text = "Data Manager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEntries)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelAgency_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            LoadDemoData();
+            this.btnAdd.Click += BtnAdd_Click;
+            this.btnUpdate.Click += BtnUpdate_Click;
+            this.btnDelete.Click += BtnDelete_Click;
+        }
+
+        private void LoadDemoData()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Title", typeof(string));
+            dt.Columns.Add("Category", typeof(string));
+            dt.Columns.Add("Status", typeof(string));
+            dt.Columns.Add("Date Created", typeof(DateTime));
+            dt.Columns.Add("Actions", typeof(string));
+
+            dt.Rows.Add(1, "Project Phoenix", "Development", "Active", new DateTime(2023, 1, 15), "View");
+            dt.Rows.Add(2, "Task Omega", "Design", "Completed", new DateTime(2023, 2, 20), "View");
+            dt.Rows.Add(3, "Feature Sigma", "Product", "Pending", new DateTime(2023, 3, 10), "View");
+            dt.Rows.Add(4, "Bug Lambda", "QA", "Resolved", new DateTime(2023, 4, 5), "View");
+            dt.Rows.Add(5, "Enhancement Delta", "Development", "Active", new DateTime(2023, 5, 12), "View");
+            dt.Rows.Add(6, "Documentation Psi", "Documentation", "Completed", new DateTime(2023, 6, 18), "View");
+            dt.Rows.Add(7, "Meeting Chi", "Management", "Scheduled", new DateTime(2023, 7, 22), "View");
+            dt.Rows.Add(8, "Review Phi", "Design", "In Review", new DateTime(2023, 8, 1), "View");
+            dt.Rows.Add(9, "Release Gamma", "Product", "Released", new DateTime(2023, 9, 8), "View");
+            dt.Rows.Add(10, "Support Zeta", "Support", "Open", new DateTime(2023, 10, 15), "View");
+
+            this.dataGridViewEntries.DataSource = dt;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            var dt = this.dataGridViewEntries.DataSource as DataTable;
+            if (dt == null) return;
+            int newId = dt.Rows.Count + 1;
+            dt.Rows.Add(newId, $"New Entry {newId}", "Category", "Status", DateTime.Now, "View");
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridViewEntries.SelectedRows.Count > 0)
+            {
+                var row = this.dataGridViewEntries.SelectedRows[0];
+                row.Cells[1].Value = "[Updated] " + row.Cells[1].Value;
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update.");
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewEntries.SelectedRows.Count > 0)
+            {
+                dataGridViewEntries.Rows.RemoveAt(dataGridViewEntries.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+
+        private void labelAppName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
